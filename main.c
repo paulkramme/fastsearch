9e63ae405618f22b02e8029bdf2bb92878dab231
Paul Kramme <pjkramme@gmail.com>
2017-01-14 22:52:45 +0100

    Add working directory browse code

--- main.c
+++ main.c
@@ -8,14 +8,13 @@
 #define VERSION "v0.1"
 #define MAX_FILENAME_LENGH 256
 #define MAX_PATH_LENGH 4096
-#define PERMS 0777
 
 int main(int argc, char *argv[])
 {
 	DIR *path;
 	char searched_file[MAX_FILENAME_LENGH];
 	char searched_in_path[MAX_PATH_LENGH];
-	int pointer_to_dirent_array = 0;
+	struct dirent *filestuff;
 
 	printf("FASTSCAN %s\n", VERSION);
 	if(argc < 3)
@@ -23,11 +22,21 @@ int main(int argc, char *argv[])
 		puts("You gave too few arguments. Aborting.");
 		return 1;
 	}
+
 	strcpy(searched_file, argv[2]);
 	strcpy(searched_in_path, argv[1]);
 	path = opendir(searched_in_path);
-	pointer_to_dirent_array = readdir(path);
-	print("NAME: %s", pointer_to_dirent_array.d_name[]);
+	for(;;)
+	{
+		filestuff = readdir(path);
+		if(filestuff == NULL)
+		{
+			printf("Nothing left\n");
+			break;
+		}
+		printf("NAME: %s\n", filestuff->d_name);
+		printf("INODE: %lu\n", filestuff->d_ino);
+	}
 	closedir(path);
 	return 0;
 }
